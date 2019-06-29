ae0fac7222be1bfe4669c392fc0a837f39af3c10
A W <waq.mmhy@live.ca>
2019-06-29 16:51:46 -0400

    Initial Commit

---
+++ Project 6b/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 6b
+//
+//  Created by Angela Wu  on 2019-06-29.
+//  Copyright © 2019 Angela Wu . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
